2867928f907263b94890055f588b138f2a904ce2
Austin Farmer <austinfarmer954@gmail.com>
2022-05-29 19:31:43 -0400

    Removed MODEM_CHECK
    wanna use wireless? you should have a modem

--- General/wireless.lua
+++ General/wireless.lua
@@ -17,19 +17,15 @@ local function formatMessage(type, message)
 end
 
 local function broadcastMessage(type, message)
-	if MODEM_CHECK then
-		local formattedMsg = formatMessage(type, message)
-		MODEM.transmit(1, 1, formattedMsg)
-	end
+	local formattedMsg = formatMessage(type, message)
+	MODEM.transmit(1, 1, formattedMsg)
 end
 
 local function receiveMessage(type)
-	if MODEM_CHECK then
-		while true do
-			local event, side, recFreq, replyFreq, resp, dist = os.pullEvent("modem_message")
-			if resp.type == type then
-				return resp.computerId, resp.computerName, resp.message
-			end
+	while true do
+		local event, side, recFreq, replyFreq, resp, dist = os.pullEvent("modem_message")
+		if resp.type == type then
+			return resp.computerId, resp.computerName, resp.message
 		end
 	end
 end
@@ -37,14 +33,11 @@ end
 -- FUNCTIONS & STATIC DATA ABOVE
 -- PROGRAM RUN ORDER STARTS HERE
 
-MODEM_CHECK = false
-
 local pList = peripheral.getNames()
 for index, p in ipairs(pList) do
 	if peripheral.getType(p) == "modem" then
 		MODEM = peripheral.wrap(p)
 		MODEM.open(1)
-		MODEM_CHECK = true
 	end
 end
 
