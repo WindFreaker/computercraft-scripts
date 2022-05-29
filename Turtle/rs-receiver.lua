f2399fe687dd6df342506824e42f6e6e20aab40c
Austin Farmer <austinfarmer954@gmail.com>
2022-05-29 19:27:17 -0400

    Removed copied code and whitespace

--- Turtle/rs-receiver.lua
+++ Turtle/rs-receiver.lua
@@ -1,26 +1,11 @@
 -- https://raw.githubusercontent.com/WindFreaker/computercraft-tests/master/Turtle/rs-receiver.lua
 
-local function formatMessage(type, message)
-	local computerName = os.getComputerLabel()
-	if computerName == nil then
-		computerName = ""
-	end
 
-	local formatted = {
-		["type"] = type,
-		["computerId"] = os.getComputerID(),
-		["computerName"] = computerName,
-		["message"] = message
-	}
-
-	return formatted
-end
 
 -- FUNCTIONS & STATIC DATA ABOVE
 -- PROGRAM RUN ORDER STARTS HERE
 
 WIRELESS = require("wireless")
-
 
 print("Turning on all sides...")
 
